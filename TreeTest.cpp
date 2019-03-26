cff2b5853c52824d342f42869529f24740a7c11a
Elmalem <44621326+Elmalem@users.noreply.github.com>
2019-03-26 12:27:57 +0200

    Update TreeTest.cpp

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -19,13 +19,13 @@ int main() {
   threetree.insert(3);
 
 
-  // //my test
-  // ariel::Tree x;
-  // x.insert(6).insert(5).insert(7).insert(4);
-  // ariel::Tree y;
-  // ariel::Tree z;
-  // z.insert(1);
-  // //
+   //my test
+  ariel::Tree x;
+  x.insert(6).insert(5).insert(7).insert(4);
+  ariel::Tree y;
+  ariel::Tree z;
+  z.insert(1);
+  //
   badkan::TestCase tc("Binary tree");
   tc
   .CHECK_EQUAL (emptytree.size(), 0)
@@ -44,24 +44,24 @@ int main() {
   .CHECK_EQUAL (threetree.right(5), 7)
   .CHECK_THROWS(threetree.insert(3))
   .CHECK_OK    (threetree.print())
-  // //my test
-  // .CHECK_EQUAL(x.size(),4)
-  // .CHECK_EQUAL(x.root(),6)
-  // .CHECK_EQUAL(x.parent(5),6)
-  // .CHECK_EQUAL(x.left(6),5)
-  // .CHECK_EQUAL(x.right(6),7)
-  // .CHECK_EQUAL(x.parent(7),6)
-  // .CHECK_OK(x.remove(4))
-  // .CHECK_OK(x.insert(9))
-  // .CHECK_THROWS(x.insert(5))
-  // //x end and y start here
-  // .CHECK_EQUAL(y.size(),0)
-  // .CHECK_THROWS(y.root())
-  // .CHECK_THROWS(y.print())
-  // //y end and z start here
-  // .CHECK_EQUAL(z.size(),1)
-  // .CHECK_EQUAL(z.root(),1)
-  // //
+   //my test
+  .CHECK_EQUAL(x.size(),4)
+  .CHECK_EQUAL(x.root(),6)
+  .CHECK_EQUAL(x.parent(5),6)
+  .CHECK_EQUAL(x.left(6),5)
+  .CHECK_EQUAL(x.right(6),7)
+  .CHECK_EQUAL(x.parent(7),6)
+  .CHECK_OK(x.remove(4))
+  .CHECK_OK(x.insert(9))
+  .CHECK_THROWS(x.insert(5))
+  //x end and y start here
+  .CHECK_EQUAL(y.size(),0)
+  .CHECK_THROWS(y.root())
+  .CHECK_THROWS(y.print())
+  //y end and z start here
+  .CHECK_EQUAL(z.size(),1)
+  .CHECK_EQUAL(z.root(),1)
+  //
   .print();
 
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
